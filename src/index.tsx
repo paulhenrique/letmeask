fa74c1f9a7a5fccd70a719978754c8f243883585
Paulo Henrique <phvcandido@gmail.com>
2021-06-25 19:36:33 -0300

    Atualizando versão para publicação

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import './services/firebase';
+
+import './styles/global.scss';
+
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
