8cc65916ac796a88fe78b8ac4e071a15b5ace980
Paulo Henrique <phvcandido@gmail.com>
2021-06-25 19:09:25 -0300

    Configurando o router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { Room } from "./pages/Room";
+import { AdminRoom } from "./pages/AdminRoom";
+
+import { AuthContextProvider } from "./contexts/AuthContext";
 function App() {
   return (
-    <div className="App">
-      <h1>Let Me Ask</h1>
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
+        </Switch>
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
